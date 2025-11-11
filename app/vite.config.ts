0110f3f6ec91e0c4ec127b38f10c2314b93397ff
AdiAs <adi7878787@gmail.com>
2025-11-11 15:37:29 +0100

    Set up Docker to development

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+const isDocker = process.env.DOCKER === 'true'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: isDocker ? '0.0.0.0' : 'localhost',
+    port: 5173,
+  },
 })
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,12 +6,12 @@ import routes from "./routes/index.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
